08b9e6872a23d205a9b8c87585e84d843c909751
teacher <teacher@192.168.2.14>
2020-04-20 21:17:45 +0800

    Java

--- src/main/java/com/myjava/ocp/lab01/MaskSystem2.java
+++ src/main/java/com/myjava/ocp/lab01/MaskSystem2.java
@@ -1,5 +1,17 @@
 package com.myjava.ocp.lab01;
 
+import java.util.stream.Stream;
+
 public class MaskSystem2 {
-    
+    public static void main(String[] args) {
+        Mask m1 = new Mask("Red", true);
+        Mask m2 = new Mask("Red", false);
+        Mask m3 = new Mask("Blue", true);
+        
+        // Java 8 串流(Stream)分析
+        Stream.of(m1, m2, m3).forEach(m -> {
+            System.out.printf("color: %s price: %d %s\n", m.getColor(), Mask.price, m.isChild()?"兒童":"成人");
+        });
+        
+    }
 }
