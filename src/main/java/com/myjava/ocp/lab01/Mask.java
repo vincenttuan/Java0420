2abcb7749e6165ffc2024deb864e61b67a88bcd0
teacher <teacher@192.168.2.14>
2020-04-20 20:50:27 +0800

    Java

--- src/main/java/com/myjava/ocp/lab01/Mask.java
+++ src/main/java/com/myjava/ocp/lab01/Mask.java
@@ -2,17 +2,22 @@ package com.myjava.ocp.lab01;
 
 public class Mask {
     private String color;
-    private int price;
+    private boolean child;
+    public final static int price = 5;
+    
     public void setColor(String c) {
         color = c;
-    }
-    public void setPrice(int p) {
-        price = p;
     }
     public String getColor() {
         return color;
     }
-    public int getPrice() {
-        return price;
+
+    public boolean isChild() {
+        return child;
     }
+
+    public void setChild(boolean child) {
+        this.child = child;
+    }
+    
 }
--- src/main/java/com/myjava/ocp/lab01/MaskSystem.java
+++ src/main/java/com/myjava/ocp/lab01/MaskSystem.java
@@ -4,11 +4,11 @@ public class MaskSystem {
     public static void main(String[] args) {
         Mask m1 = new Mask();
         m1.setColor("Blue");
-        m1.setPrice(5);
+        m1.setChild(true);
         Mask m2 = new Mask();
         m2.setColor("Red");
-        m2.setPrice(5);
-        System.out.printf("color: %s price: %d\n", m1.getColor(), m1.getPrice());
-        System.out.printf("color: %s price: %d\n", m2.getColor(), m2.getPrice());
+        m2.setChild(false);
+        System.out.printf("color: %s price: %d %s\n", m1.getColor(), Mask.price, m1.isChild()?"兒童":"成人");
+        System.out.printf("color: %s price: %d %s\n", m2.getColor(), Mask.price, m2.isChild()?"兒童":"成人");
     }
 }
