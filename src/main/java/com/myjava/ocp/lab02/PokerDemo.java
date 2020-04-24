1c9f4387dee24b950903f70b5eb4ae81ae995338
teacher <teacher@192.168.2.14>
2020-04-24 21:09:37 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab02/PokerDemo.java
@@ -0,0 +1,7 @@
+package com.myjava.ocp.lab02;
+// ♠♥♦♣
+public class PokerDemo {
+    public static void main(String[] args) {
+        String[] poker = {"♠A", "♠2", "♠3", "♠4", "♠5", "♠6", "♠7", "♠8", "♠9", "♠10", "♠J", "♠Q", "♠K"};
+    }
+}
