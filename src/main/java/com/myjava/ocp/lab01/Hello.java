bc306c26e0a52c68ee05c54976cd940034eac4db
teacher <teacher@192.168.2.14>
2020-04-24 17:27:09 +0800

    Java

--- src/main/java/com/myjava/ocp/lab01/Hello.java
+++ src/main/java/com/myjava/ocp/lab01/Hello.java
@@ -2,6 +2,6 @@ package com.myjava.ocp.lab01;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 }
