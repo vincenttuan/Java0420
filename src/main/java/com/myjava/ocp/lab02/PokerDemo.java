bd258d05ce33e876fb193f8318f12e5e22a3102a
teacher <teacher@192.168.2.14>
2020-04-24 21:19:33 +0800

    Java

--- src/main/java/com/myjava/ocp/lab02/PokerDemo.java
+++ src/main/java/com/myjava/ocp/lab02/PokerDemo.java
@@ -9,7 +9,19 @@ public class PokerDemo {
             "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K",
         };
         print(poker);
+        shuffle(poker);
+        print(poker);
     }
+    
+    public static void shuffle(String[] poker) {
+        int pos1 = 0;
+        int pos2 = 1;
+        String pos1Value = poker[pos1];
+        String pos2Value = poker[pos2];
+        poker[pos1] = pos2Value;
+        poker[pos2] = pos1Value;
+    }
+    
     public static void print(String[] poker) {
         for(String p : poker) {
             System.out.print(p + " ");
