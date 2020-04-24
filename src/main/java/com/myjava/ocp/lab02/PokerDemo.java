c5fd127a69eb6d583a7cbcf1570431041a9678a6
teacher <teacher@192.168.2.14>
2020-04-24 21:24:29 +0800

    Java

--- src/main/java/com/myjava/ocp/lab02/PokerDemo.java
+++ src/main/java/com/myjava/ocp/lab02/PokerDemo.java
@@ -1,5 +1,8 @@
 package com.myjava.ocp.lab02;
 // ♠♥♦♣
+
+import java.util.Random;
+
 public class PokerDemo {
     public static void main(String[] args) {
         String[] poker = {
@@ -9,17 +12,20 @@ public class PokerDemo {
             "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K",
         };
         print(poker);
-        shuffle(poker);
+        shuffle(poker, 10000);
         print(poker);
     }
     
-    public static void shuffle(String[] poker) {
-        int pos1 = 0;
-        int pos2 = 1;
-        String pos1Value = poker[pos1];
-        String pos2Value = poker[pos2];
-        poker[pos1] = pos2Value;
-        poker[pos2] = pos1Value;
+    public static void shuffle(String[] poker, int count) {
+        Random r = new Random();
+        for(int i=1;i<=count;i++) {
+            int pos1 = r.nextInt(poker.length);
+            int pos2 = r.nextInt(poker.length);
+            String pos1Value = poker[pos1];
+            String pos2Value = poker[pos2];
+            poker[pos1] = pos2Value;
+            poker[pos2] = pos1Value;
+        }
     }
     
     public static void print(String[] poker) {
