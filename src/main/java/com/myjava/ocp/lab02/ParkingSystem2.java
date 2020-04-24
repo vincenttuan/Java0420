49e8206e1ddaeee4f72e78b39e400df1902c8606
teacher <teacher@192.168.2.14>
2020-04-24 19:53:49 +0800

    Java

--- src/main/java/com/myjava/ocp/lab02/ParkingSystem2.java
+++ src/main/java/com/myjava/ocp/lab02/ParkingSystem2.java
@@ -1,17 +1,17 @@
 package com.myjava.ocp.lab02;
 
-public class ParkingSystem {
+public class ParkingSystem2 {
     public static void main(String[] args) {
         int parking = 0b101010;
         System.out.println("車位狀態: " + Integer.toBinaryString(parking));
-        int no = 2;
+        int no = 3;
         System.out.print("no=" + no + "車位, ");
         no = (int)Math.pow(2, no);
         int check = parking & no;
-        System.out.println(check == 0 ? "可停車" : "不可停車");
-        if (check == 0) {
-            // 停車
-            parking += no;
+        System.out.println(check > 0 ? "可移車" : "不可移車");
+        if (check > 0) {
+            // 移車
+            parking -= no;
             System.out.println("車位狀態: " + Integer.toBinaryString(parking));
         }
         
