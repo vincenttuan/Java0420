5ad5ed58707a80db5cf974c730c45499c6400ddf
teacher <teacher@192.168.2.14>
2020-04-24 20:46:55 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab02/ArrayDemo2.java
@@ -0,0 +1,11 @@
+package com.myjava.ocp.lab02;
+
+public class ArrayDemo2 {
+    public static void main(String[] args) {
+        int[] score = {100, 90, 80};
+        for(int i=0;i<score.length;i++) {
+            System.out.print(score[i] + "\t");
+        }
+        System.out.println();
+    }
+}
