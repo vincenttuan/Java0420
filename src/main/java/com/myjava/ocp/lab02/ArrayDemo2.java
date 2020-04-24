5778bb82f25677f6136506685cf90bb2a13abf0a
teacher <teacher@192.168.2.14>
2020-04-24 20:51:05 +0800

    Java

--- src/main/java/com/myjava/ocp/lab02/ArrayDemo2.java
+++ src/main/java/com/myjava/ocp/lab02/ArrayDemo2.java
@@ -4,7 +4,11 @@ public class ArrayDemo2 {
     public static void main(String[] args) {
         int[] score = {100, 90, 80};
         for(int i=0;i<score.length;i++) {
-            System.out.print(score[i] + "\t");
+            System.out.print(i + ":" + score[i] + "\t");
+        }
+        System.out.println();
+        for(int i=score.length-1;i >= 0;i--) {
+            System.out.print(i + ":" + score[i] + "\t");
         }
         System.out.println();
     }
