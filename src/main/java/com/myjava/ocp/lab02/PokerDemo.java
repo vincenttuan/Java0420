12462c56282234c78fd94adb4ff6c113dcea05bf
teacher <teacher@192.168.2.14>
2020-04-24 21:16:25 +0800

    Java

--- src/main/java/com/myjava/ocp/lab02/PokerDemo.java
+++ src/main/java/com/myjava/ocp/lab02/PokerDemo.java
@@ -2,6 +2,18 @@ package com.myjava.ocp.lab02;
 // ♠♥♦♣
 public class PokerDemo {
     public static void main(String[] args) {
-        String[] poker = {"♠A", "♠2", "♠3", "♠4", "♠5", "♠6", "♠7", "♠8", "♠9", "♠10", "♠J", "♠Q", "♠K"};
+        String[] poker = {
+            "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", 
+            "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", 
+            "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K",
+            "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K",
+        };
+        print(poker);
+    }
+    public static void print(String[] poker) {
+        for(String p : poker) {
+            System.out.print(p + " ");
+        }
+        System.out.println();
     }
 }
