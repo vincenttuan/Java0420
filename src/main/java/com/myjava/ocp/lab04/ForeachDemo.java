9963a0a3832d5f178ec20b4b2dcf7ab003a008d3
teacher <teacher@192.168.2.14>
2020-05-01 20:52:47 +0800

    Java

--- src/main/java/com/myjava/ocp/lab04/ForeachDemo.java
+++ src/main/java/com/myjava/ocp/lab04/ForeachDemo.java
@@ -8,12 +8,17 @@ public class ForeachDemo {
         int sum = 0;
         for(int[] num : nums) {
             for(int n : num) {
-                sum += n;
+                if(n > 25) {
+                    sum += n;
+                }
             }
         }
         System.out.println(sum);
         // flat 扁平, Map 轉換
-        int sum2 = Arrays.stream(nums).flatMapToInt(num -> Arrays.stream(num)).sum();
+        int sum2 = Arrays.stream(nums)
+                .flatMapToInt(num -> Arrays.stream(num))
+                .filter(n -> n >25)
+                .sum();
         System.out.println(sum2);
     }
 }
