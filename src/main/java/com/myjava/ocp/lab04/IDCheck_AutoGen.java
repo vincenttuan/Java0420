f90f9a1e2cf38ec648fcb1ad44b0dbc7ca5e0c00
teacher <teacher@192.168.2.14>
2020-05-01 20:02:01 +0800

    Java

--- src/main/java/com/myjava/ocp/lab04/IDCheck_AutoGen.java
+++ src/main/java/com/myjava/ocp/lab04/IDCheck_AutoGen.java
@@ -1,11 +1,13 @@
 package com.myjava.ocp.lab04;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class IDCheck_AutoGen {
     public static void main(String[] args) {
         String id = getId();
         System.out.println(id);
+        
     }
     
     public static String getId() {
@@ -15,5 +17,25 @@ public class IDCheck_AutoGen {
             id += new Random().nextInt(10);
         }
         return id;
+    }
+    
+    public static boolean check(String id) {
+        int[] idNums = new int[11];
+        int[] delta = {1, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1};
+        for(int i=0;i<id.length();i++) {
+            switch (id.charAt(i)) {
+                case 'A':
+                    idNums[0] = 1;idNums[1] = 0;
+                    break;
+                default: // 0~9
+                    idNums[i+1] = id.charAt(i) - 48;
+            }
+        }
+        // idNums * delta
+        int sum = 0;
+        for(int i=0;i<idNums.length;i++) {
+            sum += idNums[i] * delta[i];
+        }
+        return sum % 10 == 0;
     }
 }
