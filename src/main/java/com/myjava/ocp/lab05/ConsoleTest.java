7d441484370400cbbeaf61ba1dce8123288bb637
teacher <teacher@192.168.2.14>
2020-05-04 20:56:29 +0800

    Java

--- src/main/java/com/myjava/ocp/lab05/ConsoleTest.java
+++ src/main/java/com/myjava/ocp/lab05/ConsoleTest.java
@@ -5,6 +5,7 @@ import java.io.Console;
 public class ConsoleTest {
     public static void main(String[] args) {
         Console cs = System.console();
+        System.out.println(cs);
         System.out.println("請輸入密碼:");
         String pass1 = cs.readLine();
         System.out.println("您輸入的密碼是: " + pass1);
