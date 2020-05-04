cc60a4ce252eb9fa5cc405002eb7eff8e96e55cd
teacher <teacher@192.168.2.14>
2020-05-04 20:50:30 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab05/ConsoleTest.java
@@ -0,0 +1,16 @@
+package com.myjava.ocp.lab05;
+
+import java.io.Console;
+
+public class ConsoleTest {
+    public static void main(String[] args) {
+        Console cs = System.console();
+        System.out.println("請輸入密碼:");
+        String pass1 = cs.readLine();
+        System.out.println("您輸入的密碼是: " + pass1);
+        
+        System.out.println("請輸入密碼:");
+        char[] pass2 = cs.readPassword();
+        System.out.println("您輸入的密碼是: " + new String(pass2));
+    }
+}
