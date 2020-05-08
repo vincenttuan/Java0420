529ad7d175a09766a5e6368de8621fa5502bdfe6
teacher <teacher@192.168.2.14>
2020-05-08 20:55:57 +0800

    Java

--- src/main/java/com/myjava/ocp/lab06/Computer.java
+++ src/main/java/com/myjava/ocp/lab06/Computer.java
@@ -1,6 +1,6 @@
 package com.myjava.ocp.lab06;
 
-public class Computer {
+public class Computer extends Machine{
     String name = "電腦";
     void play() {
         System.out.println("執行電腦程式");
--- src/main/java/com/myjava/ocp/lab06/Phone.java
+++ src/main/java/com/myjava/ocp/lab06/Phone.java
@@ -1,6 +1,6 @@
 package com.myjava.ocp.lab06;
 
-public class Phone {
+public class Phone extends Machine{
     String name = "手機";
     void play() {
         System.out.println("打電話");
