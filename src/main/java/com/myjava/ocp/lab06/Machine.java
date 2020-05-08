7a2e70be35294c0c473c6e51d85c039124fa94b5
teacher <teacher@192.168.2.14>
2020-05-08 20:55:10 +0800

    Java

--- src/main/java/com/myjava/ocp/lab06/Machine.java
+++ src/main/java/com/myjava/ocp/lab06/Machine.java
@@ -1,8 +1,11 @@
 package com.myjava.ocp.lab06;
 
 public class Machine {
-    String name;
+    String name = "機器";
     void eat() {
         System.out.println("我吃電");
+    }
+    void play() {
+        System.out.println("我會動");
     }
 }
