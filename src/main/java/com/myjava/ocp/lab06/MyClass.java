112a560a7fa7f3974bd12b70d53206cea36a6767
teacher <teacher@192.168.2.14>
2020-05-08 19:36:41 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab06/MyClass.java
@@ -0,0 +1,14 @@
+package com.myjava.ocp.lab06;
+
+public class MyClass {
+    int x = 10;
+    static int y = 20;
+    
+    void add() {
+        x++;
+    }
+    
+    static void print() {
+        System.out.println("y=" + y);
+    }
+}
---
+++ src/main/java/com/myjava/ocp/lab06/Product.java
@@ -0,0 +1,11 @@
+package com.myjava.ocp.lab06;
+
+public class Product {
+    int stock; // 庫存
+    void sell(int amount) {
+        stock -= amount;
+    }
+    void buy(int amount) {
+        stock += amount;
+    }
+}
