13ee84173d2cb7f9277559275e104b7b408e9c05
teacher <teacher@192.168.2.14>
2020-05-11 20:32:13 +0800

    Java

--- src/main/java/com/myjava/ocp/lab07/Zoo2.java
+++ src/main/java/com/myjava/ocp/lab07/Zoo2.java
@@ -1,17 +1,35 @@
 package com.myjava.ocp.lab07;
 
+import java.util.Random;
+
 public class Zoo2 {
     public static void main(String[] args) {
         Dog dog = new Dog("柴犬");
         Cat cat = new Cat("波斯貓");
         Tiger tiger = new Tiger("老虎");
-        print(tiger);
+        print(cat);
         
     }
     
     public static void print(Animal animal) {
         System.out.println(animal.getName());
         animal.shout();
+        if(animal instanceof Tiger) {
+            ((Tiger)animal).hunt();
+        }
+    }
+    
+    public static Animal getAnimal() {
+        int n = new Random().nextInt(4); // 0~3
+        switch(n) {
+            case 0:
+                return new Dog("狼犬");
+            case 1:
+                return new Cat("花貓");
+            case 2:
+                return new Tiger("小虎");
+        }
+        return new Animal();
     }
     
 }
