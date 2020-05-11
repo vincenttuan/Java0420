c3b19ef92de11b091a92a4ad181ae163f84b5d70
teacher <teacher@192.168.2.14>
2020-05-11 21:25:34 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab07/VarageDemo2.java
@@ -0,0 +1,19 @@
+package com.myjava.ocp.lab07;
+
+import java.util.stream.IntStream;
+
+public class VarageDemo2 {
+    public static void main(String[] args) {
+        System.out.println(add(10, 20));
+    }
+    
+    public static int add(int... nums) {
+        return IntStream.of(nums).sum();
+    }
+    
+    public static int add(int x, int y) {
+        return x * y;
+    }
+    
+    
+}
