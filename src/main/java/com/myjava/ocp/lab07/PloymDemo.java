423d0aa28aa67147dd21cdc12bda368785c2789c
teacher <teacher@192.168.2.14>
2020-05-11 20:50:37 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab07/PloymDemo.java
@@ -0,0 +1,30 @@
+package com.myjava.ocp.lab07;
+
+class Base {
+    public void test() {
+        System.out.println("Base ");
+    }
+}
+class DerivedA extends Base {
+    public void test() {
+        System.out.println("DerivedA ");
+    }
+}
+class DerivedB extends DerivedA {
+    public void test() {
+        System.out.println("DerivedB ");
+    }
+}
+
+public class PloymDemo {
+    public static void main(String[] args) {
+        Base b1 = new DerivedB();
+        Base b2 = new DerivedA();
+        Base b3 = new DerivedB();
+        b1 = (Base) b3;
+        Base b4 = (DerivedA)b3;
+        b1.test();
+        b4.test();
+        
+    }
+}
