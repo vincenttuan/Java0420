ac18ecaf9c478c122a8960bf2f4d395901017be1
teacher <teacher@192.168.2.14>
2020-05-11 21:24:03 +0800

    Java

--- src/main/java/com/myjava/ocp/lab07/VarageDemo.java
+++ src/main/java/com/myjava/ocp/lab07/VarageDemo.java
@@ -1,5 +1,21 @@
 package com.myjava.ocp.lab07;
 
+import java.util.stream.IntStream;
+
 public class VarageDemo {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(add(nums));
+        System.out.println(add(10, 20, 30, 40, 50, 60));
+        System.out.println(add(10, 20, 30));
+        System.out.println(add(10, 20));
+        System.out.println(add(10));
+        System.out.println(add());
+    }
+    
+    public static int add(int... nums) {
+        return IntStream.of(nums).sum();
+    }
+    
     
 }
