5e0ac8bf4d39b55d29c3783cffd0f8c3f0c935c1
teacher <teacher@192.168.2.14>
2020-05-15 21:23:25 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab08/ExamDemo.java
@@ -0,0 +1,27 @@
+package com.myjava.ocp.lab08;
+
+import java.util.Arrays;
+
+public class ExamDemo {
+    public static void main(String[] args) {
+        int[][] exams = {{100, 90}, {50, 80}, {70, 100}};
+        
+        double chineseAvg = Arrays.asList(exams)
+                            .stream()
+                            .mapToInt(e -> e[0])
+                            .average()
+                            .getAsDouble();
+        System.out.println(chineseAvg);
+        
+        int[] exam = {100, 90};
+        System.out.println(Arrays.stream(exam).average().getAsDouble());
+        
+        double classAvg = Arrays.asList(exams)
+               .stream()
+               .mapToDouble(e -> Arrays.stream(e).average().getAsDouble())
+               .average()
+               .getAsDouble();
+        System.out.println(classAvg);
+        
+    }
+}
