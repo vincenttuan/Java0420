52aaf88fc4d6d46450fb7684c851610702d4d35a
teacher <teacher@192.168.2.14>
2020-05-15 20:46:25 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab08/power/PowerMain.java
@@ -0,0 +1,20 @@
+package com.myjava.ocp.lab08.power;
+
+import com.myjava.ocp.lab08.power.spec.Price;
+import java.util.Arrays;
+
+public class PowerMain {
+    public static void main(String[] args) {
+        // 計算出 Computer, Phone, BMW, ToyCar 的總成本(cost)
+        Price[] prices = {new Computer(), new Phone(), new BMW(), new ToyCar()};
+        // Java 7
+        int sum = 0;
+        for(Price p : prices) {
+            sum += p.getCost();
+        }
+        System.out.println(sum);
+        // Java 8
+        int sum2 = Arrays.asList(prices).stream().mapToInt(Price::getCost).sum();
+        System.out.println(sum2);
+    }
+}
