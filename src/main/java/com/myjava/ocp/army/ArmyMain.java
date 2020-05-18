beae7d7e0f5470565b0b14382a3a1346c0ce3399
teacher <teacher@192.168.2.14>
2020-05-18 21:25:49 +0800

    Java

--- src/main/java/com/myjava/ocp/army/ArmyMain.java
+++ src/main/java/com/myjava/ocp/army/ArmyMain.java
@@ -14,16 +14,20 @@ public class ArmyMain {
         car2.move();
         if (car1 instanceof Weapon) {
             Weapon w = (Weapon) car1;
+            w.copyright();
             w.shoot();
         }
         if (car2 instanceof Weapon) {
             Weapon w = (Weapon) car2;
+            w.copyright();
             w.shoot();
         }
         if (airplane instanceof Weapon) {
             Weapon w = (Weapon) airplane;
+            w.copyright();
             w.shoot();
         }
+        
 
     }
 }
--- src/main/java/com/myjava/ocp/army/Weapon.java
+++ src/main/java/com/myjava/ocp/army/Weapon.java
@@ -1,6 +1,8 @@
 package com.myjava.ocp.army;
 
 public interface Weapon {
-
     public void shoot();
+    public default void copyright() {
+        System.out.println("Vincent 製作...");
+    }
 }
