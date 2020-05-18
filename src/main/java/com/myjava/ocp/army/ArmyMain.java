8535bfad776f86ec45cf6beb7779b36cc1b4c8e8
teacher <teacher@192.168.2.14>
2020-05-18 21:09:54 +0800

    Java

--- src/main/java/com/myjava/ocp/army/ArmyMain.java
+++ src/main/java/com/myjava/ocp/army/ArmyMain.java
@@ -6,6 +6,24 @@ public class ArmyMain {
     }
 
     public static void main(String[] args) {
-        
+        Airplane airplane = new Fighter();
+        airplane.move();
+        Car car1 = new Tank();
+        Car car2 = new Truck();
+        car1.move();
+        car2.move();
+        if (car1 instanceof Weapon) {
+            Weapon w = (Weapon) car1;
+            w.shoot();
+        }
+        if (car2 instanceof Weapon) {
+            Weapon w = (Weapon) car2;
+            w.shoot();
+        }
+        if (airplane instanceof Weapon) {
+            Weapon w = (Weapon) airplane;
+            w.shoot();
+        }
+
     }
 }
