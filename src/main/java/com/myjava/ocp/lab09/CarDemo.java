0f0855b3fc489a40b42e2aeccc97fc1cfdcac7c7
teacher <teacher@192.168.2.14>
2020-05-18 20:18:44 +0800

    Java

--- src/main/java/com/myjava/ocp/lab09/CarDemo.java
+++ src/main/java/com/myjava/ocp/lab09/CarDemo.java
@@ -3,6 +3,9 @@ package com.myjava.ocp.lab09;
 public class CarDemo {
     public static void main(String[] args) {
         Car[] cars = {new Tank("坦克"), new Truck("卡車")};
-        
+        for(Car car : cars) {
+            System.out.println(car.getName());
+            car.move();
+        }
     }
 }
