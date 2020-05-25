e96cc632afd5b56c83f9a46ec1d7abd820a34529
teacher <teacher@192.168.2.14>
2020-05-25 21:22:23 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab10/Lambdademo.java
@@ -0,0 +1,20 @@
+package com.myjava.ocp.lab10;
+
+interface Calc {
+    boolean check(int n);
+}
+
+public class Lambdademo {
+    public static void main(String[] args) {
+        int[] scores = {100, 50, 70, 40};
+    }
+    
+    public static void checkScore(int[] scores, Calc calc) {
+        for(int score : scores) {
+            if(calc.check(score)) {
+                System.out.println(score);
+            }
+        }
+    }
+    
+}
