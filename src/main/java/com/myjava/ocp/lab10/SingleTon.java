ce878e941eac42ad0ef737eed30168303ae6cf78
teacher <teacher@192.168.2.14>
2020-05-25 19:48:40 +0800

    Java

--- src/main/java/com/myjava/ocp/lab10/SingleTon.java
+++ src/main/java/com/myjava/ocp/lab10/SingleTon.java
@@ -4,13 +4,16 @@ import java.util.Date;
 
 class Teacher {
     private String name = "Mary " + new Date().getTime();
+    // 自行提供(new) Teacher 物件實體 -> _instance
     private static final Teacher _instance = new Teacher();
+    // 將建構子設為 private
     private Teacher() {
 
     }
     public String getName() {
         return name;
     }
+    // 透過該方法將 Teacher 物件實體 傳出
     public static Teacher getInstance() {
         return _instance;
     }
@@ -18,10 +21,10 @@ class Teacher {
 
 public class SingleTon {
     public static void main(String[] args) throws Exception {
-        Teacher t = new Teacher();
+        Teacher t = Teacher.getInstance();
         System.out.println(t.getName());
         Thread.sleep(100);
-        Teacher t2 = new Teacher();
+        Teacher t2 = Teacher.getInstance();
         System.out.println(t2.getName());
     }
 }
