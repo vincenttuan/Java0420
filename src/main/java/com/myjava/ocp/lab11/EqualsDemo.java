d0be2b8a063f43f1cc3d890db8f8e83062aea7bc
teacher <teacher@192.168.2.14>
2020-05-29 19:51:54 +0800

    Java

--- src/main/java/com/myjava/ocp/lab11/EqualsDemo.java
+++ src/main/java/com/myjava/ocp/lab11/EqualsDemo.java
@@ -3,11 +3,12 @@ package com.myjava.ocp.lab11;
 public class EqualsDemo {
     public static void main(String[] args) {
         Pen p1 = new Pen("Red", 10);
-        Pen p2 = p1;
+        Pen p2 = new Pen("Red", 10);
         System.out.println(p1);
         System.out.println(p2);
         System.out.println(p1 == p2);
         System.out.println(p1.equals(p2));
-        
+        System.out.println(p1.hashCode());
+        System.out.println(p2.hashCode());
     }
 }
