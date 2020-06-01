018adb4c5b29fb4d578aa80d34ea078959a1e1c0
teacher <teacher@192.168.2.14>
2020-06-01 21:05:10 +0800

    Java

--- src/main/java/com/myjava/ocp/lab12/TreeSetDemo2.java
+++ src/main/java/com/myjava/ocp/lab12/TreeSetDemo2.java
@@ -1,9 +1,14 @@
 package com.myjava.ocp.lab12;
 
+import java.util.TreeSet;
+
 public class TreeSetDemo2 {
     public static void main(String[] args) {
         Exam e1 = new Exam("國文", 80);
         Exam e2 = new Exam("數學", 100);
         Exam e3 = new Exam("英文", 90);
+        TreeSet<Exam> scores = new TreeSet<>();
+        scores.add(e1);scores.add(e2);scores.add(e3);
+        System.out.println(scores);
     }
 }
