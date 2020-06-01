162e5c023fc77e5ae12cc36032847750aaeed33b
teacher <teacher@192.168.2.14>
2020-06-01 18:49:17 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab12/Paper.java
@@ -0,0 +1,14 @@
+package com.myjava.ocp.lab12;
+
+public class Paper {
+    private String size;
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+    
+}
---
+++ src/main/java/com/myjava/ocp/lab12/Pen.java
@@ -0,0 +1,16 @@
+package com.myjava.ocp.lab12;
+
+public class Pen {
+    private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    
+    
+}
