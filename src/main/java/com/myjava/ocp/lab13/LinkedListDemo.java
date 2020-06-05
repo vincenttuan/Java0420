fdf3e954481d4eba8c4ff7671970b60da547d366
teacher <teacher@192.168.2.14>
2020-06-05 18:55:40 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab13/LinkedListDemo.java
@@ -0,0 +1,31 @@
+package com.myjava.ocp.lab13;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        list.add(100);
+        list.add(0, 80);
+        list.add(90);
+        System.out.println(list);
+        System.out.println(list.size());
+        // 輪詢顯示集合內容
+        for(Integer i : list) {
+            System.out.println(i);
+        }
+        ListIterator<Integer> iter = list.listIterator();
+        while (iter.hasNext()) {
+            int index = iter.nextIndex();
+            Integer value = iter.next();
+            System.out.printf("%d : %d\n", index, value);
+        }
+        while (iter.hasPrevious()) {
+            int index = iter.previousIndex();
+            Integer value = iter.previous();
+            System.out.printf("%d : %d\n", index, value);
+        }
+    }
+}
