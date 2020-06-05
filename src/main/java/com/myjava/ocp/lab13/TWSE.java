9d67af0b21229b58f5ffff8d0a8c578ff02217e1
teacher <teacher@192.168.2.14>
2020-06-05 21:28:35 +0800

    Java

--- src/main/java/com/myjava/ocp/lab13/TWSE.java
+++ src/main/java/com/myjava/ocp/lab13/TWSE.java
@@ -9,6 +9,13 @@ public class TWSE {
         String pathtring = "src\\main\\java\\com\\myjava\\ocp\\lab13\\BWIBBU_d.csv";
         File file = new File(pathtring);
         String csvstring = new Scanner(file).useDelimiter("\\A").next();
-        System.out.println(csvstring);
+        //System.out.println(csvstring);
+        String[] rows = csvstring.split("\n");
+        for(String row : rows) {
+            if(row.trim().split(",").length == 7) {
+                System.out.println(row.trim());
+            }
+        }
+        
     }
 }
