395a05381f8fad1fbb2fadbec8023bd3e80ddd9d
teacher <teacher@192.168.2.14>
2020-06-05 20:02:05 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab13/Rice.java
@@ -0,0 +1,16 @@
+package com.myjava.ocp.lab13;
+
+public class Rice {
+    public String Title;
+    public String 編號;
+    public String 品名;
+    public String 國際條碼;
+    public String 廠商名稱;
+    public String 廠商地址;
+    public String 檢驗結果;
+    public String 不合格原因;
+    public String 違反規定;
+    public String 行政處分;
+    public String Log_UpdateTime;
+
+}
--- src/main/java/com/myjava/ocp/lab13/RiceDemo.java
+++ src/main/java/com/myjava/ocp/lab13/RiceDemo.java
@@ -6,24 +6,25 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
-public class BadRiceDemo {
+public class RiceDemo {
     public static void main(String[] args) throws Exception {
         // 1. 抓取資料源
-        String urlpath = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx";
+        //String urlpath = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx";
+        String urlpath = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceQualified.aspx";
         URL url = new URL(urlpath);
         String jsonstring = new Scanner(url.openStream(), "UTF-8").useDelimiter("\\A").next();
         //System.out.println(jsonstring);
         // 2. 將 json 透過 Gson 轉成 BadRice[]
-        BadRice[] badRices = new Gson().fromJson(jsonstring, BadRice[].class);
+        Rice[] rices = new Gson().fromJson(jsonstring, Rice[].class);
         // 建立 list 容器, 並將 badRices 注入
-        List<BadRice> list = new LinkedList<>();
-        for(BadRice badRice : badRices) {
-            list.add(badRice);
+        List<Rice> list = new LinkedList<>();
+        for(Rice rice : rices) {
+            list.add(rice);
         }
         //System.out.println(list);
         // 3. 分析
         list.stream()
-                .filter(badrice -> badrice.品名.contains("日本"))
-                .forEach(badrice -> System.out.printf("%s %s\n", badrice.品名, badrice.不合格原因));
+                .filter(rice -> rice.品名.contains("日本"))
+                .forEach(rice -> System.out.printf("%s %s\n", rice.品名, rice.不合格原因));
     }
 }
