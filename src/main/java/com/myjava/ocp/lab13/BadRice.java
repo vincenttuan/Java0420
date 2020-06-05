cca209eab560bcb468645d6579f2b94fecb1e404
teacher <teacher@192.168.2.14>
2020-06-05 19:49:56 +0800

    Java

--- src/main/java/com/myjava/ocp/lab13/BadRice.java
+++ src/main/java/com/myjava/ocp/lab13/BadRice.java
@@ -12,4 +12,11 @@ public class BadRice {
     public String 違反規定;
     public String 行政處分;
     public String Log_UpdateTime;
+
+    @Override
+    public String toString() {
+        return "BadRice{" + "Title=" + Title + ", \u7de8\u865f=" + 編號 + ", \u54c1\u540d=" + 品名 + ", \u570b\u969b\u689d\u78bc=" + 國際條碼 + ", \u5ee0\u5546\u540d\u7a31=" + 廠商名稱 + ", \u5ee0\u5546\u5730\u5740=" + 廠商地址 + ", \u6aa2\u9a57\u7d50\u679c=" + 檢驗結果 + ", \u4e0d\u5408\u683c\u539f\u56e0=" + 不合格原因 + ", \u9055\u53cd\u898f\u5b9a=" + 違反規定 + ", \u884c\u653f\u8655\u5206=" + 行政處分 + ", Log_UpdateTime=" + Log_UpdateTime + '}';
+    }
+    
+    
 }
--- src/main/java/com/myjava/ocp/lab13/BadRiceDemo.java
+++ src/main/java/com/myjava/ocp/lab13/BadRiceDemo.java
@@ -1,6 +1,9 @@
 package com.myjava.ocp.lab13;
 
+import com.google.gson.Gson;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BadRiceDemo {
@@ -11,6 +14,13 @@ public class BadRiceDemo {
         String jsonstring = new Scanner(url.openStream(), "UTF-8").useDelimiter("\\A").next();
         System.out.println(jsonstring);
         // 2. 將 json 透過 Gson 轉成 BadRice[]
-        
+        BadRice[] badRices = new Gson().fromJson(jsonstring, BadRice[].class);
+        // 建立 list 容器, 並將 badRices 注入
+        List<BadRice> list = new LinkedList<>();
+        for(BadRice badRice : badRices) {
+            list.add(badRice);
+        }
+        System.out.println(list);
+        // 3. 分析
     }
 }
