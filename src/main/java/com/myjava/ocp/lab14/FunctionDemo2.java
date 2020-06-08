b21e0a95459774af3f5a0f2a709d65cea6c213cf
teacher <teacher@192.168.2.14>
2020-06-08 21:00:52 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab14/FunctionDemo2.java
@@ -0,0 +1,32 @@
+package com.myjava.ocp.lab14;
+
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class FunctionDemo2 {
+    public static void main(String[] args) {
+        // 1. Consumer
+        Consumer<Integer> consumer = (t)->System.out.println(t + t);
+        consumer.accept(10);
+        consumer.accept(20);
+        // 2. Function
+        Function<Integer, Double> func = (t) -> t * t * 3.14;
+        System.out.println(func.apply(5));
+        System.out.println(func.apply(10));
+        // 3. Predicate
+        Predicate<Integer> pre = (t) -> t >= 60?true:false;
+        System.out.println(pre.test(80));
+        System.out.println(pre.test(40));
+        // 4. Supplier
+        Supplier<String> sup = () -> "20.8";
+        System.out.println(sup.get());
+        // 5. BiFunction
+        BiFunction<Integer, Integer, Double> bf = (h, w) -> w / Math.pow(h/100.0, 2);
+        System.out.printf("bmi: %.2f\n", bf.apply(170, 60));
+    }
+    
+    
+}
