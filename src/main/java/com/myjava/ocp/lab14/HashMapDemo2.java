11cf8179b94ba63d4b1f152684ce5a4a8b02fa1d
teacher <teacher@192.168.2.14>
2020-06-08 20:29:28 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab14/HashMapDemo2.java
@@ -0,0 +1,17 @@
+package com.myjava.ocp.lab14;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class HashMapDemo2 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> exam = new HashMap<>();
+        exam.put("國文", 100);
+        exam.put("數學", 80);
+        exam.put("英文", 100);
+        System.out.println(exam);
+        exam.computeIfPresent("數學", (k, v) -> v + 10);
+        System.out.println(exam);
+    }
+}
