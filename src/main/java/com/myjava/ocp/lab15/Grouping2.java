3fa4ab185102391dbe90fd56a9391ce59cca419a
teacher <teacher@192.168.2.14>
2020-06-12 19:04:49 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab15/Grouping2.java
@@ -0,0 +1,8 @@
+package com.myjava.ocp.lab15;
+
+public class Grouping2 {
+    public static void main(String[] args) {
+        String[] fruits = {"apple", "apple", "banana", "watermelon", "apple", 
+                           "orange", "watermelon", "banana", "coconut"};
+    }
+}
