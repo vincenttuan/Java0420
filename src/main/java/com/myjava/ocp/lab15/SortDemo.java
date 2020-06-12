31d3d657ff6707c2aece78d2c6b308df7af784b7
teacher <teacher@192.168.2.14>
2020-06-12 20:49:03 +0800

    Java

--- src/main/java/com/myjava/ocp/lab15/SortDemo.java
+++ src/main/java/com/myjava/ocp/lab15/SortDemo.java
@@ -1,5 +1,7 @@
 package com.myjava.ocp.lab15;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -10,6 +12,19 @@ public class SortDemo {
         IntStream.range(0, 10).forEach(i -> nums[i] = new Random().nextInt(100));
         Stream.of(nums).forEach(num -> System.out.print(num + " "));
         System.out.println();
+        // 排序 Java 7
+        Arrays.sort(nums, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+        Stream.of(nums).forEach(num -> System.out.print(num + " "));
+        System.out.println();
         
+        // 排序 Java 8
+        Arrays.sort(nums, (o1, o2) -> o1 - o2);
+        Stream.of(nums).forEach(num -> System.out.print(num + " "));
+        System.out.println();
     }
 }
