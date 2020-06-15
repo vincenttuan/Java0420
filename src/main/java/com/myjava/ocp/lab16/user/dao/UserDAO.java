aca7c64c3867f2bae38bb4d262e4ebfc9f29df88
teacher <teacher@192.168.2.14>
2020-06-15 19:56:35 +0800

    Java

--- src/main/java/com/myjava/ocp/lab16/user/dao/UserDAO.java
+++ src/main/java/com/myjava/ocp/lab16/user/dao/UserDAO.java
@@ -1,5 +1,26 @@
 package com.myjava.ocp.lab16.user.dao;
 
+import com.google.gson.Gson;
+import com.myjava.ocp.lab16.user.po.User;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class UserDAO {
+    public User[] query() {
+        File file = new File("src\\main\\java\\com\\myjava\\ocp\\lab16\\user\\db\\users.json");
+        String json = null;
+        try {
+            json = new Scanner(file).useDelimiter("//A").next();
+        } catch (FileNotFoundException ex) {
+            System.out.println("users.json 檔案位置不正確, " + ex);
+            return null;
+        }
+        User[] users = new Gson().fromJson(json, User[].class);
+        return users;
+    }
+    
     
 }
