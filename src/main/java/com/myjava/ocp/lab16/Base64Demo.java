959a5ca0d989f8b09904285237ba58380ecb625c
teacher <teacher@192.168.2.14>
2020-06-15 21:01:18 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab16/Base64Demo.java
@@ -0,0 +1,10 @@
+package com.myjava.ocp.lab16;
+
+import java.util.Base64;
+
+public class Base64Demo {
+    public static void main(String[] args) throws Exception {
+        String base64String = Base64.getUrlEncoder().encodeToString("1234".getBytes("utf-8"));
+        System.out.println(base64String);
+    }
+}
