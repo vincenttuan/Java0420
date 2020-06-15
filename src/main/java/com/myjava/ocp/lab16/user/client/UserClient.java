1a907376a67946c68c51ef96cb866e1b1bbe5ce1
teacher <teacher@192.168.2.14>
2020-06-15 20:09:04 +0800

    Java

--- src/main/java/com/myjava/ocp/lab16/user/client/UserClient.java
+++ src/main/java/com/myjava/ocp/lab16/user/client/UserClient.java
@@ -1,5 +1,17 @@
 package com.myjava.ocp.lab16.user.client;
 
+import com.myjava.ocp.lab16.user.service.UserService;
+import java.util.Scanner;
+
 public class UserClient {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("請輸入 username:");
+        String username = sc.next();
+        System.out.println("請輸入 password:");
+        Integer password = sc.nextInt();
+        UserService userService = new UserService();
+        boolean check = userService.login(username, password);
+        System.out.println(check ? "登入成功" : "登入失敗");
+    }
 }
