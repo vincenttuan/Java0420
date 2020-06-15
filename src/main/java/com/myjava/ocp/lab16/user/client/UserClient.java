8ab6ee814704d5efdff3d9508bfb3b84d8a64522
teacher <teacher@192.168.2.14>
2020-06-15 20:11:35 +0800

    Java

--- src/main/java/com/myjava/ocp/lab16/user/client/UserClient.java
+++ src/main/java/com/myjava/ocp/lab16/user/client/UserClient.java
@@ -1,17 +1,24 @@
 package com.myjava.ocp.lab16.user.client;
 
 import com.myjava.ocp.lab16.user.service.UserService;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserClient {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("請輸入 username:");
-        String username = sc.next();
-        System.out.println("請輸入 password:");
-        Integer password = sc.nextInt();
-        UserService userService = new UserService();
-        boolean check = userService.login(username, password);
-        System.out.println(check ? "登入成功" : "登入失敗");
+        try {
+            System.out.println("請輸入 username:");
+            String username = sc.next();
+            System.out.println("請輸入 password:");
+            Integer password = sc.nextInt();
+            UserService userService = new UserService();
+            boolean check = userService.login(username, password);
+            System.out.println(check ? "登入成功" : "登入失敗");
+        } catch (InputMismatchException e) {
+            System.out.println("輸入錯誤(密碼請輸入數字) !");
+        }
+
     }
 }
