fe04b4325a017fd0775d19ed43d56b7410ec1916
teacher <teacher@192.168.2.14>
2020-06-15 20:05:26 +0800

    Java

--- src/main/java/com/myjava/ocp/lab16/user/service/UserService.java
+++ src/main/java/com/myjava/ocp/lab16/user/service/UserService.java
@@ -1,5 +1,18 @@
 package com.myjava.ocp.lab16.user.service;
 
+import com.myjava.ocp.lab16.user.dao.UserDAO;
+import com.myjava.ocp.lab16.user.po.User;
+import java.util.stream.Stream;
+
 public class UserService {
-    
+    private UserDAO dao = new UserDAO();
+    public boolean login(String username, Integer password) {
+        User[] users = dao.query();
+        if(users == null) {
+            return false;
+        }
+        return Stream.of(users)
+                .anyMatch(u -> u.getUsername().equals(username) && u.getPassword().equals(password));
+                
+    }
 }
