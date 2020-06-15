c6ebcb9118e16d4ce6d38d24730c62b8f59617cc
teacher <teacher@192.168.2.14>
2020-06-15 21:19:29 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab16/exchange/Exchange.java
@@ -0,0 +1,23 @@
+package com.myjava.ocp.lab16.exchange;
+
+public class Exchange {
+    private String type;
+    private Double balance;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+    
+}
