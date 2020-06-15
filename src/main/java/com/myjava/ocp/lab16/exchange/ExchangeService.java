144cfb98caa9c9ae9590b342de3039dddafa0de1
teacher <teacher@192.168.2.14>
2020-06-15 21:23:51 +0800

    Java

--- src/main/java/com/myjava/ocp/lab16/exchange/ExchangeService.java
+++ src/main/java/com/myjava/ocp/lab16/exchange/ExchangeService.java
@@ -1,5 +1,20 @@
 package com.myjava.ocp.lab16.exchange;
 
+import com.google.gson.Gson;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ExchangeService {
-    
+    public Exchange[] query() {
+        String json = null;
+        try {
+            File file = new File("src\\main\\java\\com\\myjava\\ocp\\lab16\\exchange\\ex.json");
+            json = new Scanner(file).useDelimiter("\\A").next();
+        } catch (FileNotFoundException e) {
+            System.out.println("檔案找不到, " + e);
+            return null;
+        }
+        return new Gson().fromJson(json, Exchange[].class);
+    }
 }
