18ea71982d18aad3438e0c32f36b70f3268c03c8
teacher <teacher@192.168.2.14>
2020-06-15 21:27:34 +0800

    Java

--- src/main/java/com/myjava/ocp/lab16/ExchangeClient.java
+++ src/main/java/com/myjava/ocp/lab16/ExchangeClient.java
@@ -1,10 +1,14 @@
 
 package com.myjava.ocp.lab16;
 
+import com.myjava.ocp.lab16.exchange.ExchangeService;
+
 /**
  *
  * @author teacher
  */
 public class ExchangeClient {
-    
+    public static void main(String[] args) {
+        System.out.printf("%.2f\n", new ExchangeService().totalBalance());
+    }
 }
