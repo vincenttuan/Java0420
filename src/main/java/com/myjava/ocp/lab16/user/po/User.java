3c4fc830d62cf0fe3101fd5f16f2d54cb5880d92
teacher <teacher@192.168.2.14>
2020-06-15 19:48:55 +0800

    Java

--- src/main/java/com/myjava/ocp/lab16/user/po/User.java
+++ src/main/java/com/myjava/ocp/lab16/user/po/User.java
@@ -1,5 +1,30 @@
 package com.myjava.ocp.lab16.user.po;
 
 public class User {
+    private String username;
+    private Integer password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getPassword() {
+        return password;
+    }
+
+    public void setPassword(Integer password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "username=" + username + ", password=" + password + '}';
+    }
+    
+    
     
 }
