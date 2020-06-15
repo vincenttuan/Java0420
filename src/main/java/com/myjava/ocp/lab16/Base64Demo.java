df2e269dd8e75d5ae936b0ae08ec56315daef86a
teacher <teacher@192.168.2.14>
2020-06-15 21:02:47 +0800

    Java

--- src/main/java/com/myjava/ocp/lab16/Base64Demo.java
+++ src/main/java/com/myjava/ocp/lab16/Base64Demo.java
@@ -6,5 +6,9 @@ public class Base64Demo {
     public static void main(String[] args) throws Exception {
         String base64String = Base64.getUrlEncoder().encodeToString("1234".getBytes("utf-8"));
         System.out.println(base64String);
+        base64String = Base64.getUrlEncoder().encodeToString("5678".getBytes("utf-8"));
+        System.out.println(base64String);
+        base64String = Base64.getUrlEncoder().encodeToString("1111".getBytes("utf-8"));
+        System.out.println(base64String);
     }
 }
