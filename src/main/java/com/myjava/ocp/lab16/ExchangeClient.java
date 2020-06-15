5f27e145c6142e36f2659484efc61f537c1e796a
teacher <teacher@192.168.2.14>
2020-06-15 21:33:16 +0800

    Java

--- src/main/java/com/myjava/ocp/lab16/ExchangeClient.java
+++ src/main/java/com/myjava/ocp/lab16/ExchangeClient.java
@@ -9,6 +9,6 @@ import com.myjava.ocp.lab16.exchange.ExchangeService;
  */
 public class ExchangeClient {
     public static void main(String[] args) {
-        System.out.printf("%.2f\n", new ExchangeService().totalBalance());
+        System.out.printf("NTD $%.2f\n", new ExchangeService().totalBalance());
     }
 }
