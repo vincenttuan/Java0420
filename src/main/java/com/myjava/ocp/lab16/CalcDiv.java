246d9038fa7b20b30a6fc4e8cd581257329d5660
teacher <teacher@192.168.2.14>
2020-06-15 19:15:33 +0800

    Java

--- src/main/java/com/myjava/ocp/lab16/CalcDiv.java
+++ src/main/java/com/myjava/ocp/lab16/CalcDiv.java
@@ -1,5 +1,6 @@
 package com.myjava.ocp.lab16;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalcDiv {
@@ -9,10 +10,22 @@ public class CalcDiv {
         // 分母
         int[] y = {2, 5, 0};
         Scanner sc = new Scanner(System.in);
-        System.out.print("請選擇分母 0->2, 1->5, 2->0 : ");
-        int idx = sc.nextInt();
-        int z = 0;
-        z = x / y[idx];
-        System.out.printf("%d / %d = %d\n", x, y[idx], z);
+        System.out.println("請選擇分母 0->2, 1->5, 2->0 : ");
+        try {
+            int idx = sc.nextInt();
+            int z = 0;
+            z = x / y[idx];
+            System.out.printf("%d / %d = %d\n", x, y[idx], z); 
+        } catch (ArithmeticException e) {
+            System.out.println("數學錯誤:" + e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("陣列錯誤:" + e);
+        } catch (InputMismatchException e) {
+            System.out.println("輸入錯誤, 請輸入數字");
+        } catch (Exception e) {
+            System.out.println("其他錯誤");
+            e.printStackTrace(System.out);
+        }
+        
     }
 }
