1cdb8e744f1d3a09d519dea03db9c302b7edb8dc
teacher <teacher@192.168.2.14>
2020-06-19 18:52:36 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab16/CheckUncheck.java
@@ -0,0 +1,24 @@
+package com.myjava.ocp.lab16;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CheckUncheck {
+    public static void main(String[] args) {
+        bar();
+        try {
+            foo();
+        } catch (Exception ex) {
+            
+        }
+    }
+    
+    public static void foo() throws Exception {
+        
+    }
+    
+    public static void bar() throws ArithmeticException {
+        
+    }
+    
+}
