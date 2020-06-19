f6787d38b3cb87961aff2aca5ce53806429c89b2
teacher <teacher@192.168.2.14>
2020-06-19 21:35:40 +0800

    Java

--- src/main/java/com/myjava/ocp/lab16/MyDerby.java
+++ src/main/java/com/myjava/ocp/lab16/MyDerby.java
@@ -2,14 +2,20 @@ package com.myjava.ocp.lab16;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class MyDerby {
     public static void main(String[] args) {
         String url = "jdbc:derby://localhost:1527/sample";
         String user = "app";
         String password = "app";
-        try(Connection conn = DriverManager.getConnection(url, user, password);) {
+        String sql = "SELECT * FROM product";
+        try(Connection conn = DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)) {
             System.out.println(!conn.isClosed()); 
+            
         } catch (Exception e) {
             System.out.println(e);
         }
