f86d1c2d8f44d72904cae4b9922b4e99f191690b
teacher <teacher@192.168.2.14>
2020-06-19 21:42:59 +0800

    Java

--- src/main/java/com/myjava/ocp/lab16/MyDerby.java
+++ src/main/java/com/myjava/ocp/lab16/MyDerby.java
@@ -11,10 +11,18 @@ public class MyDerby {
         String user = "app";
         String password = "app";
         String sql = "SELECT * FROM product";
-        try(Connection conn = DriverManager.getConnection(url, user, password);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql)) {
+        try(Connection conn = DriverManager.getConnection(url, user, password); // 資料庫連線物件
+            Statement stmt = conn.createStatement(); // 資料庫 SQL語法 敘述物件
+            ResultSet rs = stmt.executeQuery(sql)) { // SQL 查詢 -> 得到結果集 ResultSet
             System.out.println(!conn.isClosed()); 
+            
+            while(rs.next()) {
+               int id = rs.getInt("PRODUCT_ID");
+               double cost = rs.getDouble("PURCHASE_COST");
+               int quantity = rs.getInt("QUANTITY_ON_HAND");
+               String desp = rs.getString("DESCRIPTION");
+                System.out.printf("%-10d %-10.1f %-10d %-30s\n", id, cost, quantity, desp);
+            }
             
         } catch (Exception e) {
             System.out.println(e);
