5b456eb1c05284ebc608fc7f5950896a4628d12d
teacher <teacher@192.168.2.14>
2020-06-22 20:45:59 +0800

    Java

--- src/main/java/com/myjava/ocp/lab17/jdbc/CreateBatchRecord.java
+++ src/main/java/com/myjava/ocp/lab17/jdbc/CreateBatchRecord.java
@@ -2,9 +2,11 @@ package com.myjava.ocp.lab17.jdbc;
 
 // 建立批次資料列
 
+import com.github.javafaker.Faker;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.Random;
 
 public class CreateBatchRecord {
     public static void main(String[] args) {
@@ -15,8 +17,8 @@ public class CreateBatchRecord {
         try(Connection conn = DriverManager.getConnection(url, username, password);
             PreparedStatement pstmt = conn.prepareStatement(sql);) {
             
-            pstmt.setInt(1, 22);
-            pstmt.setString(2, "Tom");
+            pstmt.setInt(1, new Random().nextInt(40) + 10);
+            pstmt.setString(2, new Faker().name().firstName());
             int count = pstmt.executeUpdate();
             System.out.println("建立資料列 : " + count);
             
