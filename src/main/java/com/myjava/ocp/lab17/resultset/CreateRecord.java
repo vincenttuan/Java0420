c3e072f434f7eaf3f5b58ea9da9d33731b1e17c7
teacher <teacher@192.168.2.14>
2020-06-22 21:15:59 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab17/resultset/CreateRecord.java
@@ -0,0 +1,26 @@
+package com.myjava.ocp.lab17.resultset;
+
+// 建立資料列
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class CreateRecord {
+    public static void main(String[] args) {
+        String sql = "INSERT INTO Student(age, name) VALUES(21, 'Mary')";
+        String url = "jdbc:derby://localhost:1527/sample";
+        String username = "app";
+        String password = "app";
+        try(Connection conn = DriverManager.getConnection(url, username, password);
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Student");) {
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
