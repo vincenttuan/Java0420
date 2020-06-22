b1e3c3d8c4547cc9a212cc9769251670d4d651e4
teacher <teacher@192.168.2.14>
2020-06-22 20:01:07 +0800

    Java

--- src/main/java/com/myjava/ocp/lab17/jdbc/ReadRecord.java
+++ src/main/java/com/myjava/ocp/lab17/jdbc/ReadRecord.java
@@ -17,9 +17,12 @@ public class ReadRecord {
         try(Connection conn = DriverManager.getConnection(url, username, password);
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);) {
+            
             ResultSetMetaData md = rs.getMetaData();
             System.out.printf("%-10s%-10s%-10s\n", md.getColumnName(1), md.getColumnName(2), md.getColumnName(3));
+            
             System.out.println("------------------------------");
+            
             while(rs.next()) {
                 int id = rs.getInt("id");
                 int age = rs.getInt("age");
