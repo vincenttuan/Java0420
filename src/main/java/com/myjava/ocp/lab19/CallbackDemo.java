968782d2fc09157cea4fd39453464051e44da112
teacher <teacher@192.168.2.14>
2020-07-03 19:15:19 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab19/CallbackDemo.java
@@ -0,0 +1,30 @@
+package com.myjava.ocp.lab19;
+
+import java.util.Random;
+
+interface Box {
+    void setValue(int value);
+}
+
+class Student implements Runnable {
+    Box box;
+    public Student(Box box) {
+        this.box = box;
+    }
+    @Override
+    public void run() {
+        int value = new Random().nextInt(101);
+        box.setValue(value);
+    }
+}
+
+public class CallbackDemo {
+    public static void main(String[] args) {
+        Box box = (value) -> {
+            System.out.println("我得到 " + value);
+        };
+        Student student = new Student(box);
+        Thread t = new Thread(student);
+        t.start();
+    }
+}
