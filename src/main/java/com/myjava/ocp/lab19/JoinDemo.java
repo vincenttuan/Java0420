2cc9d1220e1a2abd144de2d040b4e15c38d4decc
teacher <teacher@192.168.2.14>
2020-07-03 20:03:16 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab19/JoinDemo.java
@@ -0,0 +1,31 @@
+package com.myjava.ocp.lab19;
+
+class Father extends Thread {
+    public void run() {
+        System.out.println("爸爸下班回家");
+        System.out.println("爸爸想洗熱水澡");
+        System.out.println("爸爸發現沒瓦斯了");
+        System.out.println("爸爸打電話請瓦斯工人送瓦斯");
+        Worker worker = new Worker();
+        worker.start();
+    }
+}
+
+class Worker extends Thread {
+    public void run() {
+        System.out.println("工人送瓦斯");
+        for(int i=1;i<=5;i++) {
+            System.out.printf("%d 秒鐘 ", i);
+            try {
+                Thread.sleep(i);
+            } catch (Exception e) {
+                System.out.println("工人發生意外");
+            }
+        }
+        System.out.println("\n工人瓦斯送到");
+    }
+}
+
+public class JoinDemo {
+    
+}
