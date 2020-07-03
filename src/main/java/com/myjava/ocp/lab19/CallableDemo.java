4a8866759d151e95f81c193c6d6e7017eef68d84
teacher <teacher@192.168.2.14>
2020-07-03 19:41:46 +0800

    Java

--- src/main/java/com/myjava/ocp/lab19/CallableDemo.java
+++ src/main/java/com/myjava/ocp/lab19/CallableDemo.java
@@ -3,10 +3,12 @@ package com.myjava.ocp.lab19;
 import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
 class Lotto implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
+        Thread.sleep(10000);
         int n = new Random().nextInt(100);
         return n;
     }
@@ -18,5 +20,6 @@ public class CallableDemo {
         FutureTask<Integer> task = new FutureTask<>(lotto);
         new Thread(task).start();
         System.out.println(task.get());
+        System.out.println("其他工作...");
     }
 }
