819bcdc14073aea71a33c6a0c3a64c02f39361be
teacher <teacher@192.168.2.14>
2020-07-03 21:03:35 +0800

    Java

--- src/main/java/com/myjava/ocp/lab19/OpenWeatherDemo.java
+++ src/main/java/com/myjava/ocp/lab19/OpenWeatherDemo.java
@@ -10,10 +10,10 @@ public class OpenWeatherDemo {
         try {
             String path = "https://api.openweathermap.org/data/2.5/weather?q=Taipei&appid=794612d0a2869ed6b7c78d427133a5fc";
             String json = new Scanner(new URL(path).openStream()).useDelimiter("\\A").next();
-            JsonObject jo = JsonParser.parseString(json)
-                                      .getAsJsonObject()
-                                      .get("main")
-                                      .getAsJsonObject();
+            JsonObject jo = JsonParser.parseString(json) // 剖析 json 字串
+                                      .getAsJsonObject() // 取得 json 根物件
+                                      .get("main")       // 取得 main 這個 json 元素
+                                      .getAsJsonObject();// 得到該元素的 json 物件
             System.out.println(jo);
             
         } catch (Exception e) {
