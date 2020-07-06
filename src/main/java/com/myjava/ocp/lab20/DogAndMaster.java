89eb44750f030bb4fa372efe2c933e44e0e40d9a
teacher <teacher@192.168.2.14>
2020-07-06 21:12:36 +0800

    Java

--- src/main/java/com/myjava/ocp/lab20/DogAndMaster.java
+++ src/main/java/com/myjava/ocp/lab20/DogAndMaster.java
@@ -1,11 +1,31 @@
 package com.myjava.ocp.lab20;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 class Cookies {
+    private boolean isEmpty = true;
     public synchronized void put(int n) {
+        if(!isEmpty) {
+            try {
+                wait(); // 進入等待
+            } catch (InterruptedException ex) {
+            }
+        }
         System.out.printf("主人放第 %d 塊餅乾\n", n);
+        isEmpty = false;
+        notify(); // 呼叫小狗
     }
     public synchronized void eat(int n) {
+        if(isEmpty) {
+            try {
+                wait(); // 進入等待
+            } catch (InterruptedException ex) {
+            }
+        }
         System.out.printf("小狗吃第 %d 塊餅乾\n", n);
+        isEmpty = true;
+        notify(); // 呼叫主人
     }
 }
 
