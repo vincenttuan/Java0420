0e56a19e7c39894ccbe65626c76a657fe1aade53
teacher <teacher@192.168.2.14>
2020-07-06 19:31:37 +0800

    Java

--- src/main/java/com/myjava/ocp/lab20/ATM.java
+++ src/main/java/com/myjava/ocp/lab20/ATM.java
@@ -1,6 +1,6 @@
 package com.myjava.ocp.lab20;
 
-class Account {
+class Account { // 資源物件(帳戶)
     private int money;
     public Account(int money) {
         this.money = money;
@@ -9,6 +9,7 @@ class Account {
         String tName = Thread.currentThread().getName();
         // 取得帳戶餘額
         int tmpMoney = money;
+        // 判斷帳戶餘額是否足夠 ?
         if(tmpMoney - m >= 0) {
             money = tmpMoney - m;
             System.out.printf("%s 提款 $%d 成功 餘額 $%d\n", tName, m, money);
@@ -16,6 +17,22 @@ class Account {
             System.out.printf("%s 提款 $%d 失敗(餘額不足) 餘額 $%d\n", tName, m, money);
         }
     }
+}
+
+class Withdraw implements Runnable { // 提款行為
+    private int money;
+    private Account account;
+
+    public Withdraw(int money, Account account) {
+        this.money = money;
+        this.account = account;
+    }
+    
+    @Override
+    public void run() {
+        account.withdraw(money);
+    }
+    
 }
 
 public class ATM {
