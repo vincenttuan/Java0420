087b165d42ac32ff02a75898384240842be74c67
teacher <teacher@192.168.2.14>
2020-07-06 21:04:55 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab20/DogAndMaster.java
@@ -0,0 +1,27 @@
+package com.myjava.ocp.lab20;
+
+class Cookies {
+    public synchronized void put(int n) {
+        System.out.printf("主人放第 %d 塊餅乾");
+    }
+    public synchronized void eat(int n) {
+        System.out.printf("小狗吃第 %d 塊餅乾");
+    }
+}
+
+public class DogAndMaster {
+    public static void main(String[] args) {
+        Cookies cookies = new Cookies();
+        Runnable r1 = () -> {
+            for(int i=1;i<=10;i++) {
+                cookies.put(i); // 放餅乾的工作
+            }
+        };
+        Runnable r2 = () -> {
+            for(int i=1;i<=10;i++) {
+                cookies.eat(i); // 吃餅乾的工作
+            }
+        };
+        
+    }
+}
