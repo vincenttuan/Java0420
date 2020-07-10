c95eae1ef4c059ac150ca6c799ab1906cedd171d
teacher <teacher@192.168.2.14>
2020-07-10 19:34:18 +0800

    Java

--- src/main/java/com/myjava/ocp/lab21/ExecutorDemo.java
+++ src/main/java/com/myjava/ocp/lab21/ExecutorDemo.java
@@ -2,6 +2,7 @@ package com.myjava.ocp.lab21;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 import java.util.concurrent.Executor;
 
 // 報時器
@@ -11,6 +12,13 @@ class MyTime implements Runnable {
         Date now = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         System.out.printf("現在時間: %s\n", sdf.format(now));
+    }
+}
+
+class MyLotto implements Runnable {
+    @Override
+    public void run() {
+        System.out.printf("Lotto 號碼: %d\n", new Random().nextInt(100));
     }
 }
 
@@ -26,7 +34,7 @@ public class ExecutorDemo {
     public static void main(String[] args) {
         MyExecutor exec = new MyExecutor();
         exec.execute(new MyTime());
-        exec.execute(new MyTime());
-        exec.execute(new MyTime());
+        exec.execute(new MyLotto());
+        exec.execute(() -> {System.out.println("Hello");});
     }
 }
