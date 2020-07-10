b35d2211f112b83d48b3ce581fc65070da85a52b
teacher <teacher@192.168.2.14>
2020-07-10 20:45:39 +0800

    Java

--- src/main/java/com/myjava/ocp/lab21/ExecutorDemo2.java
+++ src/main/java/com/myjava/ocp/lab21/ExecutorDemo2.java
@@ -1,5 +1,6 @@
 package com.myjava.ocp.lab21;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -11,11 +12,19 @@ class Runner implements Runnable {
     }
 }
 
+class Caller implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+        return "Caller";
+    }
+}
 public class ExecutorDemo2 {
     public static void main(String[] args) throws Exception {
         ExecutorService es = Executors.newFixedThreadPool(2);
         Future f1 = es.submit(new Runner());
+        Future f2 = es.submit(new Caller());
         System.out.println(f1.get());
+        System.out.println(f2.get());
         es.shutdown();
     }
 }
