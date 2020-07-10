088386984b44a446c5d4c4fbeb3e22535e35dce8
teacher <teacher@192.168.2.14>
2020-07-10 18:58:43 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab21/CarCyclicBarrier.java
@@ -0,0 +1,30 @@
+package com.myjava.ocp.lab21;
+
+import java.util.Random;
+import java.util.concurrent.CyclicBarrier;
+
+class Car extends Thread {
+    private CyclicBarrier cb;
+
+    public Car(CyclicBarrier cb) {
+        this.cb = cb;
+    }
+    
+    @Override
+    public void run() {
+        String tname = Thread.currentThread().getName();
+        System.out.printf("%s 從台北出發\n");
+        try {
+            Thread.sleep(new Random().nextInt(5000));
+            System.out.printf("%s 到台中了\n");
+            cb.await();
+        } catch (Exception e) {
+        }
+        System.out.printf("%s 繼續往高雄前進\n");
+    }
+    
+}
+
+public class CarCyclicBarrier {
+    
+}
