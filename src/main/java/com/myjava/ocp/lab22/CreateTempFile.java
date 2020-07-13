2b29ff133b15d98fba714f3ebe7229b18285327f
teacher <teacher@192.168.2.14>
2020-07-13 21:23:59 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab22/CreateTempFile.java
@@ -0,0 +1,12 @@
+package com.myjava.ocp.lab22;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateTempFile {
+    public static void main(String[] args) throws IOException  {
+        File path = new File("src\\main\\java\\com\\myjava\\ocp\\lab22\\files");
+        File file = File.createTempFile("temp", ".txt", path);
+        System.out.printf("暫存檔是否存在: %b\n", file.exists());
+    }
+}
