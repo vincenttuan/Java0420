24cad706d05ca7f671c1bd5579c7a2c1083c0059
teacher <teacher@192.168.2.14>
2020-07-13 19:18:11 +0800

    Java

--- src/main/java/com/myjava/ocp/lab21/SchedularDemo2.java
+++ src/main/java/com/myjava/ocp/lab21/SchedularDemo2.java
@@ -10,9 +10,13 @@ public class SchedularDemo2 {
     
     public static void main(String[] args) {
         Runnable r = () -> {
-            Date date = new Date();
+            int delay = new Random().nextInt(5000);
+            try {
+                Thread.sleep(delay);
+            } catch (Exception e) {
+            }
             int n = new Random().nextInt(9) + 1;
-            System.out.printf("%d %s\n", n, date.toString());
+            System.out.printf("%d 產生號碼所花的秒數 %.1f, 列印時間: %s\n", n, delay/1000.0, new Date());
         };
         ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
         service.scheduleWithFixedDelay(r, 1, 3, TimeUnit.SECONDS);
