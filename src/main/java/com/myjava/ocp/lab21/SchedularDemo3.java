e3b9a5cb5eb899078e6fe5fb6976891479b9de09
teacher <teacher@192.168.2.14>
2020-07-13 19:26:34 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab21/SchedularDemo3.java
@@ -0,0 +1,24 @@
+package com.myjava.ocp.lab21;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class SchedularDemo3 {
+    
+    public static void main(String[] args) {
+        Runnable r = () -> {
+            int delay = new Random().nextInt(5000);
+            try {
+                Thread.sleep(delay);
+            } catch (Exception e) {
+            }
+            int n = new Random().nextInt(9) + 1;
+            System.out.printf("%d 產生號碼所花的秒數 %.1f, 列印時間: %s\n", n, delay/1000.0, new Date());
+        };
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(r, 1, 3, TimeUnit.SECONDS);
+    }
+}
