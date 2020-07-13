063e05f65ea388e6f02387b31c023825725a7e1a
teacher <teacher@192.168.2.14>
2020-07-13 21:43:29 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab22/Lab.java
@@ -0,0 +1,10 @@
+package com.myjava.ocp.lab22;
+
+import java.io.File;
+
+public class Lab {
+    public static void main(String[] args) {
+        File dir = new File("src\\main\\java\\com\\myjava\\ocp\\lab22\\files");
+        
+    }
+}
