a437489a96f91ebce7b11d789ead59cd3449a3c0
teacher <teacher@192.168.2.14>
2020-07-13 21:18:54 +0800

    Java

--- src/main/java/com/myjava/ocp/lab22/CreateNewFile2.java
+++ src/main/java/com/myjava/ocp/lab22/CreateNewFile2.java
@@ -4,11 +4,16 @@ import java.io.File;
 
 // 建立 fruit/apple.txt
 public class CreateNewFile2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         File path = new File("src\\main\\java\\com\\myjava\\ocp\\lab22\\files\\fruit");
         System.out.printf("路徑是否存在: %b\n", path.exists());
         if(!path.exists()) {
-            System.out.printf("建立路徑: %b\n", path.mkdir());
+            boolean check = path.mkdir(); // 建立路徑
+            System.out.printf("建立路徑: %b\n", check);
+            if(check) { // 建立路徑是否成功 ?
+                File file = new File("src\\main\\java\\com\\myjava\\ocp\\lab22\\files\\fruit\\apple.txt");
+                System.out.printf("建立檔案: %b\n", file.createNewFile());
+            }
         }
         
     }
