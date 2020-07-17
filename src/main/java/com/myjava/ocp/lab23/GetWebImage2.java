328bc608211633ca66a4c0bb29c9eb8d658c2da1
teacher <teacher@192.168.2.14>
2020-07-17 20:49:01 +0800

    Java

--- src/main/java/com/myjava/ocp/lab23/GetWebImage2.java
+++ src/main/java/com/myjava/ocp/lab23/GetWebImage2.java
@@ -2,12 +2,17 @@ package com.myjava.ocp.lab23;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class GetWebImage2 {
     public static void main(String[] args) throws Exception {
         String from = "https://www.google.com/search?q=baby&tbm=isch";
         Document doc = Jsoup.connect(from).get();
         System.out.println(doc);
+        Elements elements = doc.select("img");
+        for(Element e : elements) {
+            System.out.println(e.attr("src"));
+        }
     }
 }
