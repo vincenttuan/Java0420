216530f95b443122f8fb46336565bcf116a1b6d1
teacher <teacher@192.168.2.14>
2020-07-17 21:30:21 +0800

    Java

--- src/main/java/com/myjava/ocp/lab23/zip/ReadSalary.java
+++ src/main/java/com/myjava/ocp/lab23/zip/ReadSalary.java
@@ -4,15 +4,16 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.zip.ZipInputStream;
+import javax.swing.JOptionPane;
 
 public class ReadSalary {
 
     public static void main(String[] args) {
-        String path = "src\\main\\java\\com\\myjava\\ocp\\lab23\\zip\\salary.zip";
+        String zipName = JOptionPane.showInputDialog("請輸入檔名:");
+        String path = String.format("src\\main\\java\\com\\myjava\\ocp\\lab23\\zip\\%s.zip", zipName);
         try (FileInputStream fr = new FileInputStream(path);
              ZipInputStream zip = new ZipInputStream(fr);
              Reader r = new InputStreamReader(zip, "UTF-8");) {
-
             // 取得被壓縮檔名 
             System.out.println(zip.getNextEntry().getName());
             char[] buffer = new char[1];
@@ -22,8 +23,7 @@ public class ReadSalary {
             }
             System.out.println();
         } catch (Exception e) {
-
+            System.out.println("查無檔案");
         }
-
     }
 }
