cf80f3c3f716b48a95803dc0af61f9ead1665c22
teacher <teacher@192.168.2.14>
2020-07-17 19:05:20 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab23/ReadNews.java
@@ -0,0 +1,18 @@
+package com.myjava.ocp.lab23;
+
+import java.io.FileReader;
+
+public class ReadNews {
+    public static void main(String[] args) {
+        String path = "src\\main\\java\\com\\myjava\\ocp\\lab23\\news.txt";
+        try(FileReader fr = new FileReader(path)) {
+            
+            int x;
+            while ((x = fr.read()) != -1) {                
+                System.out.print((char)x);
+            }
+            System.out.println();
+        } catch (Exception e) {
+        }
+    }
+}
