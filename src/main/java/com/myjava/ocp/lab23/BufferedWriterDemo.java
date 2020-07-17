3f46fc1ab7bd911b6fe84a54ca6b660b4f2d0a20
teacher <teacher@192.168.2.14>
2020-07-17 21:00:03 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab23/BufferedWriterDemo.java
@@ -0,0 +1,20 @@
+package com.myjava.ocp.lab23;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class BufferedWriterDemo {
+    public static void main(String[] args) {
+        String[] rows = {"apple", "banana"};
+        String path = "src\\main\\java\\com\\myjava\\ocp\\lab23\\news.txt";
+        try(FileWriter fw = new FileWriter(path, true);
+            BufferedWriter bw = new BufferedWriter(fw)) {
+            
+            for(String row : rows) {
+                bw.write("\n" + row);
+            }
+            
+        } catch (Exception e) {
+        }
+    }
+}
