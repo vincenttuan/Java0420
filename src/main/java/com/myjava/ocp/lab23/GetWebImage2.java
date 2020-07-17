1f2e8dede61f1849e4250b2e594305550fe8670a
teacher <teacher@192.168.2.14>
2020-07-17 20:38:16 +0800

    Java

--- src/main/java/com/myjava/ocp/lab23/GetWebImage2.java
+++ src/main/java/com/myjava/ocp/lab23/GetWebImage2.java
@@ -1,26 +1,13 @@
 package com.myjava.ocp.lab23;
 
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 
 public class GetWebImage2 {
     public static void main(String[] args) throws Exception {
         String from = "https://www.google.com/search?q=baby&tbm=isch";
-        URL url = new URL(from);
-        URLConnection hc = url.openConnection();
-        hc.setRequestProperty("User-Agent", "我不是機器人");
-        StringBuilder source = new StringBuilder();
-        try(InputStream is = hc.getInputStream()) {
-            int x;
-            while ((x = is.read()) != -1) {                
-                source.append((char)x);
-            }
-            System.out.println(source);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
+        Document doc = Jsoup.connect(from).get();
+        System.out.println(doc);
     }
 }
