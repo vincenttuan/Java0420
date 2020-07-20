4c0be7a650314af4cab13454d089d4dfb7a968ae
teacher <teacher@192.168.2.14>
2020-07-20 21:25:52 +0800

    Java

--- src/main/java/com/myjava/ocp/socket/MyClient.java
+++ src/main/java/com/myjava/ocp/socket/MyClient.java
@@ -1,10 +1,21 @@
 package com.myjava.ocp.socket;
 
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class MyClient {
+
     public static void main(String[] args) throws Exception {
         Socket socket = new Socket("127.0.0.1", 5000);
         System.out.println("連到 Server 端");
+        while (true) {
+            try (OutputStream output = socket.getOutputStream();) {
+                int ch;
+                while ((ch = System.in.read()) != 1) {
+                    output.write(ch);
+                }
+            } catch (Exception e) {
+            }
+        }
     }
 }
--- src/main/java/com/myjava/ocp/socket/MyServer.java
+++ src/main/java/com/myjava/ocp/socket/MyServer.java
@@ -1,13 +1,25 @@
 package com.myjava.ocp.socket;
 
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MyServer {
+
     public static void main(String[] args) throws Exception {
         ServerSocket server = new ServerSocket(5000);
         System.out.println("等待 Client 連入");
         Socket socket = server.accept(); // 等待 Client 連入
-                 
+        System.out.println("Client port: " + socket.getPort());
+        byte[] bytes = new byte[1024];
+        while (true) {
+            try(InputStream is = socket.getInputStream()) {
+                int len;
+                while ((len = is.read(bytes)) != -1) {                    
+                    System.out.write(bytes, 0, len);
+                }
+            } catch (Exception e) {
+            }
+        }
     }
 }
