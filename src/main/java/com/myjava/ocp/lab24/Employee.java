a2090fa195dd0c9f68ea7e414d05bbc2e14778df
teacher <teacher@192.168.2.14>
2020-07-20 18:50:11 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab24/Employee.java
@@ -0,0 +1,35 @@
+package com.myjava.ocp.lab24;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private String name;
+    private int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "name=" + name + ", salary=" + salary + '}';
+    }
+    
+}
