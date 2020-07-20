7f4fce0184edd378a55eed9f0d0ad889ca4585a1
teacher <teacher@192.168.2.14>
2020-07-20 19:01:12 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab24/ReadEmployee.java
@@ -0,0 +1,19 @@
+package com.myjava.ocp.lab24;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class ReadEmployee {
+    public static void main(String[] args) {
+        String path = "src\\main\\java\\com\\myjava\\ocp\\lab24\\emp2.bin";
+        try(FileInputStream fis = new FileInputStream(path);
+            ObjectInputStream ois = new ObjectInputStream(fis);) {
+            
+            // 讀出物件
+            Employee emp = (Employee)ois.readObject();
+            System.out.println(emp);
+            
+        } catch (Exception e) {
+        }
+    }
+}
