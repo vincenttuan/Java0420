06c697f112edd7538f81089bec5b85f2bbf93ea3
teacher <teacher@192.168.2.14>
2020-07-24 19:30:39 +0800

    Java

---
+++ src/main/java/com/myjava/ocp/lab25/ReadAllNio.java
@@ -0,0 +1,14 @@
+package com.myjava.ocp.lab25;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ReadAllNio {
+    public static void main(String[] args) throws Exception {
+        Path path = Paths.get("src\\main\\java\\com\\myjava\\ocp\\lab25\\data.txt");
+        List<String> list = Files.readAllLines(path);
+        System.out.println(list);
+    }
+}
